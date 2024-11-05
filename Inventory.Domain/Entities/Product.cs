5f42313160b93f378c0c45b67e10e19bff8262e5
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-11-05 09:25:54 -0500

    Add entity Product

---
+++ Inventory.Domain/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Inventory.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; } = default!;
+    }
+}
