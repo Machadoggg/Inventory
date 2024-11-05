50ca8c581889bcb31823c446ff53ad55fba31cba
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-11-05 09:49:00 -0500

    Add ProductsController

---
+++ Inventory.API/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Inventory.Application.Interfaces;
+using Inventory.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> Get() 
+        {
+            var products = _productService.GetProducts();
+            return Ok(products);
+        }
+    }
+}
