b0dcea636331d921b365ce16962404addfe660b0
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-11-05 09:42:58 -0500

    Add ProductRepository  to read Json

---
+++ Inventory.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using Inventory.Domain.Entities;
+using System.Text.Json;
+
+namespace Inventory.Infrastructure.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly string _filePath = "Data/inventory.json";
+
+
+        public List<Product> GetAllProducts()
+        {
+            if (!File.Exists(_filePath))
+                return new List<Product>();
+
+            var json = File.ReadAllText(_filePath);
+            return JsonSerializer.Deserialize<List<Product>>(json) ?? new List<Product>();
+        }
+    }
+}
