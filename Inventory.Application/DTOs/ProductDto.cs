59c6521424ae04eba4d3d71669948fb8cfa85657
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-11-13 23:09:21 -0500

    Add ProductDto class

---
+++ Inventory.Application/DTOs/ProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Inventory.Application.DTOs
+{
+    public class ProductDto
+    {
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; } = default!;
+    }
+}
